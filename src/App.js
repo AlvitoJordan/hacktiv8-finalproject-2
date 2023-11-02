8b85144d05aed81b8c0df4ca030fd47169469c4f
Syaeful Annas <syaefulannas33@gmail.com>
2023-11-02 21:10:30 +0700

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-// import { Provider } from "react-redux";
-// import Router from "./router";
-// import store from "./redux/store";
+import { Provider } from "react-redux";
+import Router from "./router";
+import store from "./redux/store";
 import React from "react";
-import Bukapedia from "./assets/Bukapedia.svg"
 import NavBar from "./components/organisms/NavBar";
 
 function App() {
   return (
-    // <NavBar />
-    <div className="flex items-center justify-between w-full px-3 py-3 mx-auto max-w-8xl lg:px-4">
-      <div className="flex items-center">
-        <div className="flex items-center justify-between">
-          <a href="/" className="flex">
-            <img src={Bukapedia} alt="logo" className="h-8 mr-3"/>
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-black">Bukapedia</span>
-          </a>
-        </div>
-      </div>
-    </div>
-  )
+    <>
+      <Provider store={store}>
+        <NavBar />
+        <Router />
+      </Provider>
+    </>
+  );
 }
-    // {/* <Provider store={store}> */}
-    //     <Router />
-    //   {/* </Provider> */}
-    // </>
 
 export default App;
