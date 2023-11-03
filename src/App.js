0822381ab93cbf5ead06ff0cead51d6ba66b7044
Syaeful Annas <syaefulannas33@gmail.com>
2023-11-03 20:40:19 +0700

    fix conflict

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { Provider } from "react-redux";
 import Router from "./router";
 import store from "./redux/store";
 import React from "react";
-
-import NavBar from "./components/organisms/NavBar";
+import { NavBar } from "./components/organisms";
+import { Footer } from "./components/molecules";
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
       <Provider store={store}>
         <NavBar />
         <Router />
+        <Footer copyright="© 2023 Hacktiv8 . All Rights Reserved" text="MSIB Kampus Merdeka" />
       </Provider>
     </>
   );
