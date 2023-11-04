d633f82ed02afb7355b528b7362c73efbdf691d6
Syaeful Annas <syaefulannas33@gmail.com>
2023-11-04 20:01:30 +0700

    integrasi product home

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,20 +1,23 @@
+import { useEffect, useState } from "react";
 import { Header, Card } from "../components/molecules";
 import { HeroSection } from "../components/organisms";
 
 const Homepage = () => {
+  const [product, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => setProducts(json));
+  }, []);
+
   return (
     <div className="mx-4">
       <HeroSection />
       <Header title="Product" />
       <div className="grid grid-cols-4 gap-4">
-        <Card title="Iphone" img="https://via.placeholder.com/600/92c952" categories="HP & Desktop" price="Rp.10000" desc="Ini Desc" />
-        <Card title="Iphone" img="" categories="HP & Desktop" price="Rp.10000" desc="Ini Desc" />
-        <Card title="Iphone" img="" categories="HP & Desktop" price="Rp.10000" desc="Ini Desc" />
-        <Card title="Iphone" img="" categories="HP & Desktop" price="Rp.10000" desc="Ini Desc" />
-        <Card title="Iphone" img="" categories="HP & Desktop" price="Rp.10000" desc="Ini Desc" />
-        <Card title="Iphone" img="" categories="HP & Desktop" price="Rp.10000" desc="Ini Desc" />
-        <Card title="Iphone" img="" categories="HP & Desktop" price="Rp.10000" desc="Ini Desc" />
-        <Card title="Iphone" img="" categories="HP & Desktop" price="Rp.10000" desc="Ini Desc" />
+        {product.map((item) => (
+          <Card key={item.id} title={item.title} img={item.image} categories={item.category} price={`$ ${item.price}`} desc={item.description} />
+        ))}
       </div>
     </div>
   );
