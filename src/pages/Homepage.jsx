aae2bf02ee8452c9aff5d12994b481b196ebcb6a
AlvitoJordan <alvitojordan12@gmail.com>
2023-11-06 10:00:01 +0700

    add responsive grid card in Homepage (user)

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -43,10 +43,21 @@ const Homepage = () => {
     <div className="mx-4">
       <HeroSection />
       <Header title="Product" />
-      <div className="grid grid-cols-4 gap-4">
-        {product.map((item) => (
-          <Card key={item.id} title={item.title} img={item.image} categories={item.category} price={`$ ${item.price}`} desc={item.description} id={item.id} onClick={() => handleCart(item)} />
-        ))}
+      <div className="flex items-center justify-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
+          {product.map((item) => (
+            <Card
+              key={item.id}
+              title={item.title}
+              img={item.image}
+              categories={item.category}
+              price={`$ ${item.price}`}
+              desc={item.description}
+              id={item.id}
+              onClick={() => handleCart(item)}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
