d8502cf6b956b7355e14595c9ff0aa604b5af310
Syaeful Annas <syaefulannas33@gmail.com>
2023-11-10 16:33:52 +0700

    add alert checkout

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import { Provider } from "react-redux";
 import Router from "./router";
 import store from "./redux/store";
 import React from "react";
-import { NavBar } from "./components/organisms";
-import { Footer } from "./components/molecules";
 
 function App() {
   return (
