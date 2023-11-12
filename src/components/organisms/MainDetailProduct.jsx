65c60d9ea2e9fecc830203e8c254aba019bea5d8
Alvito Jordan <alvitojordan12@gmail.com>
2023-11-12 22:56:29 +0700

    Add Responsive Detail Product & change Number Input arrow

--- src/components/organisms/MainDetailProduct.jsx
+++ src/components/organisms/MainDetailProduct.jsx
@@ -6,8 +6,9 @@ import { useNavigate, useParams } from "react-router-dom";
 import { NumberInput, Warning } from "../molecules";
 import { addToCart } from "../../redux/cart/Cart";
 import { Button, Image, Text } from "../atoms";
-import { Rating, ShoppingCart } from "../../assets";
+import { ShoppingCart } from "../../assets";
 import { getAPIAct } from "../../redux/fetch/Get";
+import { Rating } from "@mui/material";
 
 const Mainproducts = () => {
   const { id } = useParams();
@@ -57,27 +58,33 @@ const Mainproducts = () => {
   return (
     <>
       <div className="w-full">
-        <div className="flex justify-between w-full items-center">
+        <div className="flex flex-col lg:flex-row justify-between w-full items-center">
           {getProductbyId ? (
             <>
-              <div className="flex justify-center w-2/4">
+              <div className="flex justify-center lg:w-2/4">
                 <Image
                   src={getProductbyId.image}
                   alt={getProductbyId.title}
-                  className="rounded-lg w-[500px] h-[500px] object-contain"
+                  className="rounded-lg w-[300px] h-[300px] lg:w-[500px] lg:h-[500px] object-contain"
                 />
               </div>
-              <div className="px-5 py-5 w-2/4">
+              <div className="lg:px-5 py-5 lg:w-2/4">
                 <Text
-                  className="font-bold text-5xl text-darkgray"
+                  className="font-bold text-2xl lg:text-5xl text-darkgray"
                   text={getProductbyId.title}
                 />
                 <div className="flex flex-row gap-3 items-center text-yellow mt-2">
-                  <Rating />
-                  <Text
-                    className="text-darkgray font-semibold text-lg mt-1"
-                    text={getProductbyId.rating?.rate}
-                  />
+                  <div className="flex items-center space-x-3 my-2">
+                    <Rating
+                      defaultValue={getProductbyId.rating?.rate}
+                      precision={0.1}
+                      readOnly
+                    />
+                    <Text
+                      className="text-darkgray font-semibold text-lg mt-1"
+                      text={getProductbyId.rating?.rate}
+                    />
+                  </div>
                 </div>
                 <Text
                   className="text-2xl font-semibold my-4 text-darkgray"
@@ -87,7 +94,7 @@ const Mainproducts = () => {
                   className="text-base font-medium my-4 text-grayCS"
                   text={getProductbyId.description}
                 />
-                <div className="flex flex-row gap-5 items-center mt-5 mb-2">
+                <div className="flex flex-row gap-3 lg:gap-5 items-center mt-5 mb-2">
                   <NumberInput
                     value={inputNumber}
                     onChange={(val) => setInputNumber(val)}
