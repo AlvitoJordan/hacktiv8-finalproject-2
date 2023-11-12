4e8b4c91b4a277b4b7a6856f7d7cad09f26ed9d3
RezalMaksmi <rezalnurscc@gmail.com>
2023-11-12 22:46:53 +0700

    responsive detail product

--- src/components/organisms/MainDetailProduct.jsx
+++ src/components/organisms/MainDetailProduct.jsx
@@ -45,19 +45,19 @@ const Mainproducts = () => {
   return (
     <>
       <div className="w-full">
-        <div className="flex justify-between w-full items-center">
+        <div className="flex justify-between w-full items-center max-[768px]:flex-col max-[768px]:relative">
           {products[id - 1] !== null && (
             <>
-              <div className="flex justify-center w-2/4">
+              <div className="flex justify-center w-2/4 max-[768px]:w-full">
                 <Image
                   src={products[id - 1].image}
                   alt={products[id - 1].title}
                   className="rounded-lg w-[500px] h-[500px] object-contain"
                 />
               </div>
-              <div className="px-5 py-5 w-2/4">
+              <div className="px-5 py-5 w-2/4 max-[768px]:w-full">
                 <Text
-                  className="font-bold text-5xl text-darkgray"
+                  className="font-bold text-5xl text-darkgray max-[768px]:text-2xl"
                   text={products[id - 1].title}
                 />
                 <div className="flex flex-row gap-3 items-center text-yellow mt-2">
@@ -75,23 +75,27 @@ const Mainproducts = () => {
                   className="text-base font-medium my-4 text-grayCS"
                   text={products[id - 1].description}
                 />
-                <div className="flex flex-row gap-5 items-center mt-5 mb-2">
-                  <NumberInput
-                    value={inputNumber}
-                    onChange={(val) => setInputNumber(val)}
-                  />
+                <div className="flex flex-row gap-5 items-center mt-5 mb-2 max-[768px]:h-[100px] max-[768px]:relative ">
+                  <div className="max-[768px]:absolute max-[768px]:left-0">
+                    <NumberInput
+                      value={inputNumber}
+                      onChange={(val) => setInputNumber(val)}
+                    />
+                  </div>
                   <Text
-                    className="text-darkgray font-semibold"
+                    className="text-darkgray font-semibold max-[768px]:absolute max-[768px]:left-5 max-[768px]:top-0"
                     text={`Stock : ${products[id - 1].stock}`}
                   />
                 </div>
-                <p className="text-red-500 mb-8">{error}</p>
-                <Button
-                  type="LongPrimaryButtonWithIcon"
-                  icon={<ShoppingCart />}
-                  onClick={handleCart}
-                  text="Add to Cart"
-                />
+                <p className="text-red-500 mb-8 max-[768px]:absolute max-[768px]:left-5 max-[768px]:bottom-[-50px] max-[768px]:z-10">{error}</p>
+                <div className="max-[768px]:absolute max-[768px]:right-0 max-[768px]:bottom-1">
+                  <Button
+                    type="LongPrimaryButtonWithIcon"
+                    icon={<ShoppingCart />}
+                    onClick={handleCart}
+                    text="Add to Cart"
+                  />
+                </div>
               </div>
             </>
           )}
